ed8d88aa17eaea03c8d42857ae7474250382f75c
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-03 17:07:47 -0300

    feat: add padding, border-radius and box-shadow

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -10,6 +10,7 @@ export default createGlobalStyle`
 
   body {
     font-family: sans-serif;
+    background: #eee;
   }
 
   html, body, #root {
@@ -33,4 +34,7 @@ export const Container = styled.section`
   max-width: 360px;
   background: #fff;
   margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
