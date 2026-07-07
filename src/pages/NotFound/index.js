1322c8ff9718a8c37a8509cf07a89e172bd44b17
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-07 14:38:09 -0300

    feat: add page 404 (not found)

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import { Container } from "styles/GlobalStyles";
+
+const NotFound = () => {
+  return (
+    <Container>
+      <h1>Página não encontrada</h1>
+    </Container>
+  );
+};
+
+export default NotFound;
