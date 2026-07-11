fb94fa415b91fe821483f7162dc65074d15e19e2
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-11 17:34:06 -0300

    feat: add saga middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,12 @@
-import { legacy_createStore } from "redux";
+import { applyMiddleware, legacy_createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "store/modules/rootReducer";
+import rootSaga from "store/modules/rootSaga";
 
-const store = legacy_createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = legacy_createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
